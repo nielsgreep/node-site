dbfe39948af88a9603d4eb0e40b749ac950f8a79
Niels Greep <nielsgreep@hotmail.com>
2017-02-21 15:10:27 +0100

    added img and updated with latest solutions

---
+++ README.md
@@ -0,0 +1,6 @@
+My First Node Server by Scotch.io
+
+
+npm install
+
+npm start
--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
 var app = express();
 var port = 8080;
 
 // route our app
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+//use body parser
+app.use(bodyParser.urlencoded());
 
 var router = require('./app/routes');
 app.use('/', router);
